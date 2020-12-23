855277bc298334d4846cdcbbc3beacbc641e4155
Ersikan <julien.philippon@epitech.eu>
2020-12-23 10:11:03 +0100

    Add datasets listing

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+import os
+from flask.json import jsonify
 
 app = Flask(__name__)
+
+DATASETS_DIRECTORY = os.getenv("DATASETS_DIRECTORY")
+
+if not DATASETS_DIRECTORY:
+    raise ValueError("Missing environment variable DATASETS_DIRECTORY")
 
 
 @app.route("/")
 def home():
     return "Home"
+
+
+@app.route("/dataset")
+def get_datasets():
+    return jsonify(os.listdir(DATASETS_DIRECTORY))
