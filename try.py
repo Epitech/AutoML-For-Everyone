5f8ac59b83830e59899cbd6cb98d54b9187c17d5
ugolc <ugo.levi-cescutti@epitech.eu>
2021-01-02 18:03:07 +0100

    [UPDATE] Save an image of shap graph

--- try.py
+++ try.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import shap
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('CDM2018.csv')
 feature_names = [i for i in data.columns if data[i].dtype in [np.int64, np.int64]]
@@ -11,11 +12,12 @@ Y = data['Man of the Match'] = data['Man of the Match'].map({'No':0,'Yes':1})
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=1, train_size=0.70, test_size=0.30)
 
-tpot = TPOTClassifier(generations=5, population_size=20, cv=5, random_state=42, verbosity=2, max_time_mins=2)
+tpot = TPOTClassifier(generations=5, population_size=20, cv=5, random_state=42, verbosity=2, max_time_mins=5)
 
 tpot.fit(X_train, Y_train)
 
 explainer = shap.KernelExplainer(tpot.predict_proba, X_train, link="logit")
 
 shap_values = explainer.shap_values(X_test, nsamples=100)
-shap.summary_plot(shap_values, X_test, plot_type="bar")
+fig = shap.summary_plot(shap_values, X_test, plot_type="bar", show=False)
+plt.savefig('save.png')
