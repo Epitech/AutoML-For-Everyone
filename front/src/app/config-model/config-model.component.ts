f3eea9811a68cee496b66feb39b54bc11ae64f76
ugolc <ugo.levi-cescutti@epitech.eu>
2021-02-20 17:04:23 +0100

    [ADD] Add import in config-model.ts

--- front/src/app/config-model/config-model.component.ts
+++ front/src/app/config-model/config-model.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { configDict, scoring_classification, scoring_regression } from '../../api2';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 interface Food {
   value: string;
   viewValue: string;
 }
+
+// export type ModelDataType = {
+//   dispatch: (m: ModelType) => void;
+//   model: ModelType;
+//   type: any;
+// };
 
 @Component({
   selector: 'app-config-model',
@@ -11,6 +19,9 @@ interface Food {
   styleUrls: ['./config-model.component.css']
 })
 export class ConfigModelComponent implements OnInit {
+  configDictOptions = configDict;
+  scoringOptions = true == true ? scoring_classification : scoring_regression;
+  
   foods: Food[] = [
     {value: 'steak-0', viewValue: 'Steak'},
     {value: 'pizza-1', viewValue: 'Pizza'},
@@ -21,5 +32,25 @@ export class ConfigModelComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  // changeKey(key: string, input: EventTarget | null, isNumber = true) {
+  //   if (!input) return;
+  //   const { value } = input as HTMLInputElement;
+  //   if (key === 'config_dict') {
+  //     this.setConfigDict(value);
+  //   } else {
+  //     this.data.model[key] = isNumber ? +value : value;
+  //   }
+  //   this.data.dispatch(this.data.model);
+  //   console.log(this.data.model);
+  // }
+
+  // setConfigDict(value: string) {
+  //   if (value === 'Default') {
+  //     this.data.model.config_dict = undefined;
+  //   } else {
+  //     this.data.model.config_dict = value;
+  //   }
+  // }
 
 }
